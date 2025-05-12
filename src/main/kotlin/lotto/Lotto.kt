b65e243e34d8665e0c13ea8d99b221236eb5d157
Shinseojin <02sjin@naver.com>
2025-05-12 22:23:25 +0900

    로또번호 자동생성 구현(보너스번호 포함)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,29 @@
 package lotto
 
+import kotlin.random.Random
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
+}
+//구매금액 입력받기
+fun inputMoney() : Set<Int>{
 
-    // TODO: 추가 기능 구현
+}
+//로또번호 자동생성(보너스번호 포함)
+fun lottoMachine(): Pair<Set<Int>, Int> {
+    val numbers = mutableListOf<Int>()
+
+    while (numbers.size < 6) {
+        var number = Random.nextInt(1,46) //1~45까지
+        if(!numbers.contains(number)) {
+            numbers.add(number)
+        }
+    }
+    var bonus: Int
+    do {
+        bonus = Random.nextInt(1, 46) //1~45까지
+    }while (numbers.contains(bonus))
+    return Pair(numbers.toSet(), bonus)
 }
